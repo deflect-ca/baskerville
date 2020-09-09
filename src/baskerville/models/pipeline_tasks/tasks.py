66ecca094adb5fa89e9e9fcd91d57ee601589058
mazhurin <mazhurin@gmail.com>
2020-09-09 17:47:49 -0400

    White list logging typo fix

--- src/baskerville/models/pipeline_tasks/tasks.py
+++ src/baskerville/models/pipeline_tasks/tasks.py
@@ -1325,7 +1325,7 @@ class AttackDetection(Task):
         white_listed = df.where(F.col('white_list') == 1)
         if white_listed.count() > 0:
             self.logger.info(f'White listing {white_listed.count()} ips')
-            self.logger.show(white_listed.select('ip').show())
+            self.logger.info(white_listed.select('ip').show())
 
         df = df.withColumn('attack_prediction', F.when(
             (F.col('white_list') == 1), F.lit(0)).otherwise(F.col('attack_prediction')))
