4562e8c15d0a2753b27a30c0c2f584508a5d0948
mazhurin <mazhurin@gmail.com>
2020-09-23 15:12:24 -0400

    no counts in logging

--- src/baskerville/models/ip_cache.py
+++ src/baskerville/models/ip_cache.py
@@ -21,19 +21,19 @@ class IPCache(object):
 
         # return only the new comers from df
         self.logger.info('IP cache joining...')
-        original_count = df.count()
+        # original_count = df.count()
         df = df.join(self.cache[['ip']], on='ip', how='leftanti')
-        new_count = df.count()
-        self.logger.info(f'IP cache:, existing:{original_count - new_count}, new:{new_count}')
+        # new_count = df.count()
+        # self.logger.info(f'IP cache:, existing:{original_count - new_count}, new:{new_count}')
 
         # remove expired records
         self.logger.info('IP cache removing expired records...')
-        original_count = self.cache.count()
+        # original_count = self.cache.count()
         now = datetime.datetime.utcnow()
         expire_date = now - datetime.timedelta(seconds=self.ttl)
         self.cache = self.cache.where(F.col('ts') >= expire_date)
-        new_count = self.cache.count()
-        self.logger.info(f'IP cache size after expiration = {new_count} ({new_count - original_count})')
+        # new_count = self.cache.count()
+        # self.logger.info(f'IP cache size after expiration = {new_count} ({new_count - original_count})')
 
         # add the new increment
         self.logger.info('IP cache union...')
