c4606e2536f576472258562e6d5b3d777f233a0c
Maria Karanasou <karanasou@gmail.com>
2021-11-16 14:18:39 +0200

    get MY_POD_IP

--- src/baskerville/spark/__init__.py
+++ src/baskerville/spark/__init__.py
@@ -197,8 +197,8 @@ def get_or_create_spark_session(spark_conf):
         if spark_conf.spark_kubernetes_executor_memoryOverhead:
             conf.set('spark.kubernetes.executor.memoryOverhead', spark_conf.spark_kubernetes_executor_memoryOverhead)
 
-        conf.set('spark.kubernetes.driver.pod.name', os.environ['MY_POD_NAME'])
-        conf.set('spark.driver.host', os.environ['MY_POD_IP'])
+        conf.set('spark.kubernetes.driver.pod.name', os.environ.get('MY_POD_NAME'))
+        conf.set('spark.driver.host', os.environ.get('MY_POD_IP'))
         conf.set('spark.driver.port', 20020)
 
     spark = SparkSession.builder \
