df13622f6a1fb527fa0b2adccb65d9d9b815bce9
mazhurin <mazhurin@gmail.com>
2021-12-14 15:56:47 -0500

    Support for sending passed challenged ips to Elasticsearch

--- src/baskerville/util/elastic_writer.py
+++ src/baskerville/util/elastic_writer.py
@@ -25,15 +25,39 @@ class ElasticWriter(object):
             print('Error in ES indexing')
             print(str(ex))
 
+    def get_banjax_index_name(self, ts):
+        return f'banjax-{ts.year}.{ts.month:02}.{ts.day:02}'
+
+    def get_timestamp_string(self, ts):
+        return f'{ts.year}-{ts.month:02}-{ts.day:02}T{ts.hour:02}:{ts.minute:02}:{ts.second:02}.000Z'
+
     def write_challenge(self, ip, host, reason):
         ts = datetime.datetime.utcnow()
-        index_name = f'banjax-{ts.year}.{ts.month:02}.{ts.day:02}'
         record = {
             'trigger': 'baskerville_challenge',
             'tags': [reason],
             'http_host': host,
             'client_ip': ip,
-            '@timestamp': f'{ts.year}-{ts.month:02}-{ts.day:02}T{ts.hour:02}:{ts.minute:02}:{ts.second:02}.000Z'
+            '@timestamp': self.get_timestamp_string(ts)
         }
-        self.write(index_name, doc_type='banjax', record=record)
+        self.write(self.get_banjax_index_name(ts), doc_type='banjax', record=record)
 
+    def write_challenge_passed(self, ip, host):
+        ts = datetime.datetime.utcnow()
+        record = {
+            'trigger': 'baskerville_challenge_passed',
+            'http_host': host,
+            'client_ip': ip,
+            '@timestamp': self.get_timestamp_string(ts)
+        }
+        self.write(self.get_banjax_index_name(ts), doc_type='banjax', record=record)
+
+    def write_challenge_failed(self, ip, host):
+        ts = datetime.datetime.utcnow()
+        record = {
+            'trigger': 'baskerville_challenge_failed',
+            'http_host': host,
+            'client_ip': ip,
+            '@timestamp': self.get_timestamp_string(ts)
+        }
+        self.write(self.get_banjax_index_name(ts), doc_type='banjax', record=record)
